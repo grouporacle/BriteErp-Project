fba71914510458a7c846f13d9843ccd14fd676b5
unknown <siivriisinek@gmail.com>
2019-01-31 04:17:15 -0600

    Base v1

---
+++ src/test/java/com/BriteErp/utilities/ApplicationConstants.java
@@ -0,0 +1,20 @@
+package com.BriteErp.utilities;
+
+public class ApplicationConstants {
+//titles of all buttons in homepage
+    public static final String INBOX = "#Inbox";
+    public static final String CALENDAR= "Meetings (Week 5)";
+    public static final String NOTES = "Notes";
+    public static final String CONTACTS = "Contacts";
+    public static final String CRM = "Pipeline";
+    public static final String SALES = "Quotations";
+    public static final String WEBSITE = "Dashboard";
+    public static final String INVENTORY = "Inventory";
+    public static final String REPAIRS = "Repair Orders";
+    public static final String EVENTS = "Events";
+    public static final String EMPLOYEES = "Employees";
+    public static final String LEAVES = "Leaves Summary";
+    public static final String EXPENSES = "My Expenses to Submit";
+    public static final String MAINTENANCE = "Maintenance Teams";
+    public static final String DASHBOARDS = "My Dashboard";
+}
---
+++ src/test/java/com/BriteErp/utilities/Pages.java
@@ -0,0 +1,38 @@
+package com.BriteErp.utilities;
+
+import com.BriteErp.pages.*;
+
+public class Pages {
+    private LoginPage loginPage;
+    private CRMmodule crmModule;
+    private CustomersPage customersPage;
+    private ManagersPage managersPage;
+
+    public LoginPage login() {
+        if (loginPage == null) {
+            loginPage = new LoginPage();
+        }
+        return loginPage;
+    }
+
+    public CRMmodule crm() {
+        if (crmModule == null) {
+            crmModule = new CRMmodule();
+        }
+        return crmModule;
+    }
+
+    public CustomersPage customer() {
+        if (customersPage == null) {
+            customersPage = new CustomersPage();
+        }
+        return customersPage;
+    }
+
+    public ManagersPage manager() {
+        if (managersPage == null) {
+            managersPage = new ManagersPage();
+        }
+        return managersPage;
+    }
+}
