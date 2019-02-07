d0ceda63f737a581d8e000067f728d25a85665f0
selamey <efrem.tsehaye2002@gmail.com>
2019-02-06 20:44:06 -0800

    D2

--- src/test/java/com/BriteErp/Tests/Functional_Tests/CRM_Managing_Dep.java
+++ src/test/java/com/BriteErp/Tests/Functional_Tests/CRM_Managing_Dep.java
@@ -28,39 +28,88 @@ public class CRM_Managing_Dep extends TestBase {
  * Here starts STEPS of BRIT-2005 test case
  */
 
-        //1-Verify that Events CRM Manager can see options of activity types.
+      //1- Verify that Events CRM Manager can see options of activity types.
           pages.manager().activityType.click();
-        Assert.assertTrue(pages.manager().activityType.getText().equals("Activity Types"));
+          Assert.assertTrue(pages.manager().activityType.getText().equals("Activity Types"));
 
-        //2 –Verify that Events CRM Manager can search names by entering keyword to the search box
-        //    Then hitting enter.
+      //2 –Verify that Events CRM Manager can search names by entering keyword to the search box
+      //    Then hitting enter.
             pages.manager().serchbox.sendKeys("call");
             BrowserUtils.waitForPageToLoad(5000);
             pages.manager().serchbox.click();
 
-        //3 – Verify that Events CRM Manager should be able to update names of the activity types.
+       //3 – Verify that Events CRM Manager should be able to update names of the activity types.
             pages.manager().serchbox.sendKeys("Efi" + Keys.ENTER);
             BrowserUtils.waitForPageToLoad(2000);
             //pages.manager().click.click();
 
-        //4 –Verify Events CRM Manager should be able to create activity types.
+       //4 –Verify Events CRM Manager should be able to create activity types.
            pages.manager().create.click();
            pages.manager().name.sendKeys("Efi");
            //pages.manager().summary.sendKeys("CRM Manager "+ Keys.ENTER);
-           BrowserUtils.wait(10);
+           BrowserUtils.wait(5);
            pages.manager().save.click();
            pages.manager().activityType.click();
 
+              System.out.println("--------------------------");
 
-         //5 – Verify that Events CRM Manager can print all names of the activity types.
-         BrowserUtils.waitForClickablility(pages.manager().create, 15);
+      //5 – Verify that Events CRM Manager can print all names of the activity types.
+            BrowserUtils.waitForPageToLoad(5000);
+           for ( WebElement allName : pages.manager().tableNames){
+              System.out.println(allName.getText());
 
-         for ( WebElement allName : pages.manager().tableNames){
-             System.out.println(allName.getText());
-         }
+           }
+              System.out.println("--------------------------");
 
+      //6 – Verify that Events CRM Manager can print all table headers of the activity types.
+            BrowserUtils.waitForPageToLoad(5000);
 
+           for ( WebElement head : pages.manager().tableheaders){
+            System.out.println(head.getText());
+            }
+                System.out.println("--------------------------");
 
+      //7 – Verify that Events CRM Manager can print all Rows of the activity types.
+
+            BrowserUtils.waitForPageToLoad(5000);
+            System.out.println("Numbers of rows "+ pages.manager().printallRows.size());
+            for ( WebElement alltable : pages.manager().printallRows){
+                System.out.println(alltable.getText());
+            }
+               System.out.println("--------------------------");
+
+      //8 – Verify that Events CRM Manager can print all table of the activity types.
+
+            BrowserUtils.waitForPageToLoad(5000);
+
+            for ( WebElement alltable : pages.manager().printalltable){
+                System.out.println(alltable.getText());
+            }
+             System.out.println("--------------------------");
+
+      // 9. Verify that Events CRM Manager can print by coordinates : based on the row and collomn number
+
+            BrowserUtils.waitForPageToLoad(5000);
+            BrowserUtils.wait(15);
+
+            for(WebElement header : pages.manager().cells1 ){
+                System.out.println(header.getText());
+            }
+             System.out.println("--------------------------");
+
+      //10 Verify that Events CRM Manager can find the select check box for a given name
+
+          BrowserUtils.waitForPageToLoad(5000);
+          BrowserUtils.wait(15);
+
+          for(WebElement header2 : pages.manager().checkbox ) {
+            System.out.println(header2.getText());
+
+            System.out.println(header2.isSelected());
+            header2.click();
+            System.out.println(header2.isSelected());
+
+        }
 
 
     }
